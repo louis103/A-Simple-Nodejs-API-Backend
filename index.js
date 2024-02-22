78c05c6fe4e5b84f5c4de4bf058df3d071922832
louis@programmer <62738677+louis103@users.noreply.github.com>
2024-02-22 17:39:12 +0300

    the first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
+const productRoute = require("./routes/product.route.js");
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// routes
+app.use("/api/products", productRoute);
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node API Server");
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://<username>:<password>@<your-cluster-name>.wmuguhl.mongodb.net/<project-name>?retryWrites=true&w=majority&appName=<your-cluster-name>"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch((e) => {
+    console.log("Connection failed!" + e);
+  });
